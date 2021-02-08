bd2330a949bc46ce3c9b9d4239ce8e691ad1a60d
Mr.Han <3248996258@qq.com>
2021-02-08 17:16:20 +0800

    定时任务

---
+++ Blog.Quartz.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Quartz.Web.Controllers
+{
+    public class HomeController: Controller
+    {
+        //[Route("/")]
+        //public IActionResult getIndex()
+        //{
+        //    return View("~/pages/index.cshtml");
+        //}
+        [Route("home/index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("~/pages/index.cshtml");
+        }
+    }
+}
